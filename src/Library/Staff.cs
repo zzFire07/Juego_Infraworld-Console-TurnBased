0d7b7c3885da6d54268ebeed0f1f94f0d3af14c5
nicohf2022 <104432397+nicohf2022@users.noreply.github.com>
2022-10-20 22:14:45 -0300

    items shield.cs y staff.cs

---
+++ src/Library/Staff.cs
@@ -0,0 +1,12 @@
+namespace RoleplayGame
+{
+    public class Staff : IItems
+    {
+        public Staff()
+        {
+            this.AttackValue = 100;
+            this.DefenseValue = 100;
+            this.IsMagic = true;
+        }
+    }
+}
