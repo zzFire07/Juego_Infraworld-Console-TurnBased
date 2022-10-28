50a95ea04937245cef69a4d938347d870cb4efcb
Franco Bascialla <francobascialla@gmail.com>
2022-10-27 21:27:28 -0300

    Se arreglan Test

--- src/Library/Characters/EnemyWizard.cs
+++ src/Library/Characters/EnemyWizard.cs
@@ -2,12 +2,13 @@ namespace RoleplayGame
 {
     public class EnemyWizard : MagicCharacter, IEnemies
     {
+        public SpellsBook spellsBook { get; set; } = new SpellsBook();
+
         public EnemyWizard(string name) : base(name)
         {
             this.VictoryPoints = 25;
 
             Staff staff = new Staff();
-            SpellsBook spellsBook = new SpellsBook();
             
             this.EquipItem(staff);
             this.EquipItem(spellsBook);
